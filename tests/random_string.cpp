0bc9aec4b9c7d880649183f12279db150f63d9d1
HedgehogInTheCPP <a.rainman@gmail.com>
2026-08-17 23:58:52 +0300

    lint unfck

--- tests/random_string.cpp
+++ tests/random_string.cpp
@@ -54,6 +54,7 @@ float cygwin_strtof_l(char const *start, char **end) {
 class RandomEngine {
 public:
   RandomEngine() = delete;
+
   RandomEngine(uint64_t new_seed) : wyhash64_x_(new_seed) {}
 
   uint64_t next() {
