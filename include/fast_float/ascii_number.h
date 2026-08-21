8351519606bf916c19396a2474ec18b9866e10a9
HedgehogInTheCPP <a.rainman@gmail.com>
2026-08-21 16:33:16 +0300

    compilation fix for ancient standard. May be better to drop support for C++11, because C++14 is the fixed version of big C++11 changes.

--- include/fast_float/ascii_number.h
+++ include/fast_float/ascii_number.h
@@ -30,7 +30,7 @@ fastfloat_really_inline constexpr bool has_simd_opt() noexcept {
 // Next function can be micro-optimized, but compilers are entirely
 // able to optimize it well.
 template <typename UC>
-fastfloat_really_inline constexpr auto is_integer(UC c) noexcept {
+fastfloat_really_inline constexpr bool is_integer(UC c) noexcept {
   return static_cast<uint8_t>(c - UC('0')) <= 9;
 }
 
